5a7b5372e17286bc5e039738783af5da29d0b862
Egor <40001420+EgorLedyaev@users.noreply.github.com>
2018-08-22 16:18:25 +0300

    Update preryvanie_motor.ino

--- preryvanie_motor.ino
+++ preryvanie_motor.ino
@@ -1,13 +1,21 @@
-int motor = 5;
-const int SENSOR_MOTOR=2;
+#include <Servo.h>
+Servo motor;
+const int SENSOR_led=2;
 volatile int state = LOW;
 int a[6];
 int i=0;
 int h;
-void motor()
+
+int moto_pin = 9;    //Подключаем левый мотор к пину 7
+int js_position = 1500;  //Начальная позиция, всегда 1.5 мс для регуляторов бесколлекторных двигателей
+int max_position = 2300; //Максимальное значение ШИМ 2.3 мс
+int min_position = 800;  //Минимальное значени ШИМ 0.8 мс
+int start = 1;  //Флаг задержки запуска
+
+void motor_stop()
 {
   state = !state;
-  int sign_int = digitalRead(SENSOR_MOTOR);
+  int sign_int = digitalRead(SENSOR_led);
   Serial.println(!sign_int,'\n');
   if(!sign_int==1)
   {
@@ -31,17 +39,25 @@ if(i == 6)
 void setup()
 {
   Serial.begin(9600);
-  pinMode(motor, OUTPUT);
-  attachInterrupt(0, motor, CHANGE);
+  motor.attach(moto_pin, js_position, max_position);
+  attachInterrupt(0, motor_stop, CHANGE);
 }
  
 void loop()
 {
+    if(start == 1) {
+    motor.write(js_position);
+    delay(700);
+    start = 0;
+  }
+
 if (h==3){
-  digitalWrite(motor, state); 
+  js_position = 0;
+  motor.write(js_position);
 }
 else {
-  digitalWrite(motor, LOW);
+  js_position = 2300;
+  motor.write(js_position);
 }
 }
  
